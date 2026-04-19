7e70c8487590e2f36d77171b35267a03f8c5ca15
ingrodriguezji-hue <ing.rodriguezji@gmail.com>
2026-04-19 13:06:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,30 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker listo");
+const CACHE_NAME = "app-finanzas-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
